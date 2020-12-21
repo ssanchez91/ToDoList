f377f198da6f7a4c4f51670663bf6514be8bad4b
s.sanchez <s.sanchez@iinext.fr>
2020-12-21 16:31:07 +0100

    Analyse Blackfire + PhpUNit + create doc to explain analyse #3

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -12,7 +12,10 @@ class DefaultControllerTest extends WebTestCase
 
         $crawler = $client->request('GET', '/');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+
+
+
+        // $this->assertContains('To Do List app', $crawler->filter('.navbar-brand a')->text());
     }
 }
